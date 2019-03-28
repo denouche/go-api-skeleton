0544e5d492964466e388f54074b194e57ada1a3c
antoine.leveugle <antoine.leveugle@gmail.com>
2019-03-28 09:28:13 +0100

    fix: missing dao mock parameters

--- storage/dao/mock/database_mock_template.go
+++ storage/dao/mock/database_mock_template.go
@@ -10,21 +10,21 @@ func (db *DatabaseMock) GetAllTemplates() ([]*model.Template, error) {
 }
 
 func (db *DatabaseMock) GetTemplateByID(id string) (*model.Template, error) {
-	args := db.Called()
+	args := db.Called(id)
 	return args.Get(0).(*model.Template), args.Error(1)
 }
 
 func (db *DatabaseMock) CreateTemplate(template *model.Template) error {
-	args := db.Called()
+	args := db.Called(template)
 	return args.Error(0)
 }
 
 func (db *DatabaseMock) DeleteTemplate(id string) error {
-	args := db.Called()
+	args := db.Called(id)
 	return args.Error(0)
 }
 
 func (db *DatabaseMock) UpdateTemplate(template *model.Template) error {
-	args := db.Called()
+	args := db.Called(template)
 	return args.Error(0)
 }
--- storage/dao/mock/database_mock_users.go
+++ storage/dao/mock/database_mock_users.go
@@ -10,21 +10,21 @@ func (db *DatabaseMock) GetAllUsers() ([]*model.User, error) {
 }
 
 func (db *DatabaseMock) GetUserByID(id string) (*model.User, error) {
-	args := db.Called()
+	args := db.Called(id)
 	return args.Get(0).(*model.User), args.Error(1)
 }
 
 func (db *DatabaseMock) CreateUser(user *model.User) error {
-	args := db.Called()
+	args := db.Called(user)
 	return args.Error(0)
 }
 
 func (db *DatabaseMock) DeleteUser(id string) error {
-	args := db.Called()
+	args := db.Called(id)
 	return args.Error(0)
 }
 
 func (db *DatabaseMock) UpdateUser(user *model.User) error {
-	args := db.Called()
+	args := db.Called(user)
 	return args.Error(0)
 }
